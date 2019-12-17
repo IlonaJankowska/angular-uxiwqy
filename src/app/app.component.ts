09be580dd08d428f93ab15088f07b6927186a0ff
Ilona <ij@zut.edu.pl>
2019-12-17 17:01:13 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Wydatek, KATEGORIE } from './wydatek';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ListaWydatkowComponent } from './lista-wydatkow/lista-wydatkow.component';
+import { WydatkiService } from './wydatki.service';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, ListaWydatkowComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [WydatkiService]
 })
 export class AppModule { }
